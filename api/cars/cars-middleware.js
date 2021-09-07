86a8642fa8d983e237f4e45af4aaecd051401dea
bevanger <baileyevanger@gmail.com>
2021-09-07 16:58:22 -0600

    wrote out server page, wrote out first get request

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,5 +1,18 @@
+const e = require('express');
+const Cars = require('./cars-model');
+
 const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+  const { id } = req.params;
+  Cars.getById(id)
+    .then(car => {
+      if(car) {
+        req.car = car
+        next()
+      } else { 
+        next({ message: 'car with id <car id> is not found', status: 404 })
+      }
+    })
+    .catch(next)
 }
 
 const checkCarPayload = (req, res, next) => {
@@ -13,3 +26,5 @@ const checkVinNumberValid = (req, res, next) => {
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
 }
+
+module.exports = { checkCarId, checkCarPayload, checkVinNumberValid, checkVinNumberUnique }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,21 @@
-// DO YOUR MAGIC
+const { checkCarId, checkCarPayload, checkVinNumberValid, checkVinNumberUnique } = require('./cars-middleware');
+const Cars = require('./cars-model');
+
+const router = require('express').Router()
+
+router.get('/', (req, res, next) => {
+    Cars.getAll()
+        .then(cars => {
+            res.status(200).json(cars)
+        })
+        .catch(next)
+});
+
+router.use((err, req, res, next) => {
+    res.status(err.status || 500).json({ 
+        message: err.message,
+        stack: err.stack
+    })
+});
+
+module.exports = router
